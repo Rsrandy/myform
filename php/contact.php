102016421999eac14c0643f59e0ebd8abc702cf8
Randhir singh <46832545+Rsrandy@users.noreply.github.com>
2022-10-04 18:04:43 +0530

    email add

--- php/contact.php
+++ php/contact.php
@@ -1,7 +1,7 @@
 <?php
 $array = array("firstname" => "", "name" => "", "email" => "", "phone" => "", "message" => "", "firstnameError" => "", "nameError" => "", "phoneError" => "", "messageError" => "","isSuccess" => false);
 
-$emailto = "test@test.com";
+$emailto = "rsrandy68@gmail.com";
 
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
 
